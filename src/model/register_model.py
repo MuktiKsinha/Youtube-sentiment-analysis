8d42a1b795d1eaa91b2ab7fb16d0139cc84709bd
Muktiksinha <muktikumarsinha@gmail.com>
2025-11-22 22:34:08 +0530

    working version

---
+++ src/model/register_model.py
@@ -0,0 +1,90 @@
+import json
+import mlflow
+from mlflow.tracking import MlflowClient
+import logging
+import os
+
+
+
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri("http://ec2-51-20-65-11.eu-north-1.compute.amazonaws.com:5000/")
+
+
+# logging configuration
+logger = logging.getLogger('model_registration')
+logger.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+file_handler = logging.FileHandler('model_registration_errors.log')
+file_handler.setLevel('ERROR')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+def get_root_directory() -> str:
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.abspath(os.path.join(current_dir, '../../'))
+
+
+def load_model_info(file_path :str) -> dict:
+    "load the model info from jason file model_info.json"
+    try:
+        with open(file_path,'r') as file:
+            model_info = json.load(file)
+        logger.debug('Model info Loaded from %s',file_path)
+        return model_info
+    except FileNotFoundError:
+        logger.error('File not found : %s',file_path)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error occurred while loading the model info: %s', e)
+        raise
+
+def register_model(model_name:str,model_info: dict):
+    """Register the model to the MLflow Model Registry."""
+    try:
+        model_uri = f"runs:/{model_info['run_id']}/{model_info['model_path']}"
+
+        #register the model
+        model_version = mlflow.register_model(model_uri,model_name)
+
+        # Transition the model to "Staging" stage
+        client = MlflowClient()
+        client.transition_model_version_stage(
+            name=model_name,
+            version=model_version.version,
+            stage="Staging"
+        )
+        
+        logger.debug(f'Model {model_name} version {model_version.version} registered and transitioned to Staging.')
+    except Exception as e:
+        logger.error('Error during model registration: %s', e)
+        raise
+
+def main():
+    try:
+        root = get_root_directory()
+        models_dir = os.path.join(root, "models")
+        model_info_path = os.path.join(models_dir, "model_info.json")
+
+        model_info = load_model_info(model_info_path)
+
+        model_name = 'Youtube_chrome_plugin_model'
+        register_model(model_name,model_info)
+    except Exception as e:
+        logger.error('Failed to complete the model registration process: %s', e)
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+    
