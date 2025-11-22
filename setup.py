cf95319f542f18698f225a95012c742256509772
Muktiksinha <muktikumarsinha@gmail.com>
2025-11-22 20:24:18 +0530

    experimentation commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A chrome plugin for analyze sentiments on you tube',
+    author='mukti kumar sinha',
+    license='',
+)
